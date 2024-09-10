5a5b8c3229227c1672eb76af78dd0dd382e75bfd
dwiki <dwiki.nuridhuha@gmail.com>
2024-09-10 21:15:48 +0700

    00:35:31

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { SignUp } from "@clerk/nextjs";
+import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return <SignIn />;
 }
